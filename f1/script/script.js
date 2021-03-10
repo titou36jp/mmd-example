56c81adb6e3e4f8247431ce96160d794f01ab8fc
titou36jp <titou36jp@mediacat.ne.jp>
2021-03-10 12:56:09 +0900

    F1マシン比較

--- f1/script/script.js
+++ f1/script/script.js
@@ -1,22 +1,67 @@
+$(window).load(function () {
+  resetStyle();
+  setStyle(1);
+});
+
 $(function () {
   //1ページ
-  $(".page-1-btn").click(function () {
+  $(".iframe-contents-change-button > li:nth-child(1)").click(function () {
     document
       .getElementById("iframeId")
       .contentWindow.location.replace("./pages/page-1.html");
+    resetStyle();
+    setStyle(1);
   });
 
   //2ページ
-  $(".page-2-btn").click(function () {
+  $(".iframe-contents-change-button > li:nth-child(2)").click(function () {
     document
       .getElementById("iframeId")
       .contentWindow.location.replace("./pages/page-2.html");
+    resetStyle();
+    setStyle(2);
   });
 
   //3ページ
-  $(".page-3-btn").click(function () {
+  $(".iframe-contents-change-button > li:nth-child(3)").click(function () {
     document
       .getElementById("iframeId")
       .contentWindow.location.replace("./pages/page-3.html");
+    resetStyle();
+    setStyle(3);
   });
 });
+
+function resetStyle() {
+  //1ページ
+  $(".iframe-contents-change-button > li:nth-child(1)").css("color", "white");
+  $(".iframe-contents-change-button > li:nth-child(1)").css(
+    "text-shadow",
+    "1px 1px 1px rgba(10, 10, 10, 0.9)"
+  );
+
+  //2ページ
+  $(".iframe-contents-change-button > li:nth-child(2)").css("color", "white");
+  $(".iframe-contents-change-button > li:nth-child(2)").css(
+    "text-shadow",
+    "1px 1px 1.5px rgba(10, 10, 10, 0.9)"
+  );
+
+  //3ページ
+  $(".iframe-contents-change-button > li:nth-child(3)").css("color", "white");
+  $(".iframe-contents-change-button > li:nth-child(3)").css(
+    "text-shadow",
+    "1px 1px 1.5px rgba(10, 10, 10, 0.9)"
+  );
+}
+
+function setStyle(value) {
+  $(".iframe-contents-change-button > li:nth-child(" + value + ")").css(
+    "color",
+    "black"
+  );
+  $(".iframe-contents-change-button > li:nth-child(" + value + ")").css(
+    "text-shadow",
+    "1px 1px 1.5px rgba(230, 230, 230, 0.9)"
+  );
+}
