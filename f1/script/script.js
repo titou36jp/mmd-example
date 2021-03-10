4502159de2091138cb425d5f8629d776568f9299
titou36jp <titou36jp@mediacat.ne.jp>
2021-03-10 17:41:19 +0900

    F1マシン比較

--- f1/script/script.js
+++ f1/script/script.js
@@ -1,67 +1,97 @@
-$(window).load(function () {
-  resetStyle();
-  setStyle(1);
-});
+/**
+ * iframeコンテンツ切替
+ *
+ * @param {string}} loc ロケーション
+ */
+function changeLocation(loc) {
+  document.getElementById("iframeId").contentWindow.location.replace(loc);
+}
 
-$(function () {
-  //1ページ
-  $(".iframe-contents-change-button > li:nth-child(1)").click(function () {
-    document
-      .getElementById("iframeId")
-      .contentWindow.location.replace("./pages/page-1.html");
-    resetStyle();
-    setStyle(1);
-  });
-
-  //2ページ
-  $(".iframe-contents-change-button > li:nth-child(2)").click(function () {
-    document
-      .getElementById("iframeId")
-      .contentWindow.location.replace("./pages/page-2.html");
-    resetStyle();
-    setStyle(2);
-  });
-
-  //3ページ
-  $(".iframe-contents-change-button > li:nth-child(3)").click(function () {
-    document
-      .getElementById("iframeId")
-      .contentWindow.location.replace("./pages/page-3.html");
-    resetStyle();
-    setStyle(3);
-  });
-});
-
+/**
+ * iframe切替ボタンのスタイルをリセットする
+ */
 function resetStyle() {
-  //1ページ
+  //1ページのiframe切替ボタンスタイル（フォントカラー、ぼかし効果）をリセット
   $(".iframe-contents-change-button > li:nth-child(1)").css("color", "white");
   $(".iframe-contents-change-button > li:nth-child(1)").css(
     "text-shadow",
     "1px 1px 1px rgba(10, 10, 10, 0.9)"
   );
 
-  //2ページ
+  //2ページのiframe切替ボタンスタイル（フォントカラー、ぼかし効果）をリセット
   $(".iframe-contents-change-button > li:nth-child(2)").css("color", "white");
   $(".iframe-contents-change-button > li:nth-child(2)").css(
     "text-shadow",
     "1px 1px 1.5px rgba(10, 10, 10, 0.9)"
   );
 
-  //3ページ
+  //3ページのiframe切替ボタンスタイル（フォントカラー、ぼかし効果）をリセット
   $(".iframe-contents-change-button > li:nth-child(3)").css("color", "white");
   $(".iframe-contents-change-button > li:nth-child(3)").css(
     "text-shadow",
-    "1px 1px 1.5px rgba(10, 10, 10, 0.9)"
+    "1px 1px 1px rgba(10, 10, 10, 0.9)"
   );
 }
 
-function setStyle(value) {
-  $(".iframe-contents-change-button > li:nth-child(" + value + ")").css(
+/**
+ * iframe切替ボタンのスタイルをセットする
+ *
+ * @param {number}} elnum li兄弟要素のグループの要素番号
+ */
+function setStyle(elnum) {
+  //フォントカラー変更
+  $(".iframe-contents-change-button > li:nth-child(" + elnum + ")").css(
     "color",
     "black"
   );
-  $(".iframe-contents-change-button > li:nth-child(" + value + ")").css(
+  //ぼかし効果変更
+  $(".iframe-contents-change-button > li:nth-child(" + elnum + ")").css(
     "text-shadow",
-    "1px 1px 1.5px rgba(230, 230, 230, 0.9)"
+    "1px 1px 1px rgba(230, 230, 230, 0.9)"
   );
 }
+
+/**
+ * メイン処理
+ *
+ * @param {string}} loc ロケーション
+ * @param {number}} elnum li兄弟要素のグループの要素番号
+ */
+function mainProc(loc, elnum) {
+  changeLocation(loc);
+  resetStyle();
+  setStyle(elnum);
+}
+
+/**
+ * iframe切替ボタンクリック処理
+ */
+$(function () {
+  //1ページ目をクリック
+  $(".iframe-contents-change-button > li:nth-child(1)").click(function () {
+    //メイン処理
+    mainProc("./pages/page-1.html", 1);
+  });
+
+  //2ページ目をクリック
+  $(".iframe-contents-change-button > li:nth-child(2)").click(function () {
+    //メイン処理
+    mainProc("./pages/page-2.html", 2);
+  });
+
+  //3ページ目をクリック
+  $(".iframe-contents-change-button > li:nth-child(3)").click(function () {
+    //メイン処理
+    mainProc("./pages/page-3.html", 3);
+  });
+});
+
+/**
+ * 画像など含めすべてのコンテンツの読み込みが終わるタイミングで実行
+ *
+ * @author: equeko99
+ */
+$(window).load(function () {
+  //メイン処理
+  mainProc("./pages/page-1.html", 1);
+});
